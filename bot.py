cc977d21e2fc28ad08b7ea8745aa15cc901c7741
guninvalid <guninvalid@gmail.com>
2026-02-21 23:54:37 -0800

    sending messages at arbitrary time and actually fixing the indent

--- bot.py
+++ bot.py
@@ -5,6 +5,8 @@ import discord
 from dotenv import load_dotenv
 load_dotenv();
 TOKEN = os.getenv('DISCORD_BOT_TOKEN_DO_NOT_SHARE_EVER');
+OPEN_CHANNEL = os.getenv('OPEN_CHANNEL');
+DEBUG = True;
 
 def debug(message):
     if (DEBUG):
@@ -14,16 +16,16 @@ intents = discord.Intents.default();
 intents.message_content = True;
 client = discord.Client(intents=intents);
 
-DEBUG = True;
-
 @client.event
 async def on_ready():
-    debug(f'Bot is ready!');
+    channel = client.get_channel(1254693534863327314);
+    await channel.send('Hello burgle!');
 
 @client.event
 async def on_message(message):
     debug(f'{message.author}: {message.content}');
-        if (message.author == client.user):
-            return;
+    if (message.author == client.user):
+        return;
+    
 
 client.run(TOKEN);
