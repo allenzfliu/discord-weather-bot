213959051c0f3f9e63319ab844226256b36e50e4
guninvalid <guninvalid@gmail.com>
2026-02-21 23:21:38 -0800

    adding some skeletons to my closet

--- bot.py
+++ bot.py
@@ -3,5 +3,21 @@ import os
 import discord
 
 from dotenv import load_dotenv
-
 load_dotenv();
+TOKEN = os.getenv('DISCORD_BOT_TOKEN_DO_NOT_SHARE_EVER');
+
+def debug(message):
+    if (DEBUG):
+        print(message);
+
+intents = discord.Intents.default();
+intents.message_content = True;
+client = discord.Client(intents=intents);
+
+DEBUG = True;
+
+@client.event
+async def on_message(message):
+    debug(f'{message.author}: {message.content}');
+    if (message.author == client.user):
+        return;
